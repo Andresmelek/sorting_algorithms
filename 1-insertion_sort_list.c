c573245867b7f5a91435103f5710891606fb1de9
GiovanniAyala <op7puro@live.com>
2020-02-01 00:42:53 +0000

    Try fix that

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,38 +1,21 @@
 #include "sort.h"
-
+#include <stdio.h>
 void insertion_sort_list(listint_t **list)
 {
     listint_t *tmp = *list;
 
     tmp = tmp->next;
-    while (*list)
+    while (tmp)
     {
-        
-        if (tmp != NULL && tmp->prev->n > tmp->n)
+        if (tmp->prev->n > tmp->n)
         {
-        /*
             tmp->prev->next = tmp->next;
-            tmp->next->prev = tmp->prev;
-            tmp->prev = tmp->next->prev->prev;
-            if (tmp->prev == NULL)
-                tmp->next->prev->prev = tmp;
-            else
-            {
-                tmp->prev->prev->next = tmp;
-                
-            }
-            tmp->next = tmp->next->prev;
-            if (tmp->prev == NULL)
-            {
-                *list = tmp;
-                print_list(*list); 
-            }
-            else
-            {
-
-                print_list(*list);
-            }
-            */
+            tmp->next = tmp->prev;
+            tmp->prev = tmp->prev->prev;
+            tmp->prev->next->prev = tmp->next;
+            tmp->prev->next = tmp;
+            printf("%i\n", tmp->n);
+            print_list(*list);
         }
         tmp = tmp->next;
     }
